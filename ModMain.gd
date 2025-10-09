a8e48167670dd0151dbdac1b9b82ecc4360e9bd0
WT <go.to.another5@gmail.com>
2025-10-09 18:36:43 +0300

    0.1.4

--- ModMain.gd
+++ ModMain.gd
@@ -1,5 +1,100 @@
 extends Node
 
-func _init(mod_loader):
-	mod_loader.installScriptExtension("res://ShipTransponder/hud/ShipList.gd")
-	mod_loader.installScriptExtension("res://ShipTransponder/hud/components/ShipOnOMSNameTransponder.gd")
+const MOD_PRIORITY = 0
+const MOD_NAME = "ShipTransponder"
+const MOD_VERSION_MAJOR = 0
+const MOD_VERSION_MINOR = 1
+const MOD_VERSION_BUGFIX = 4
+const MOD_VERSION_METADATA = ""
+var modPath:String = get_script().resource_path.get_base_dir() + "/"
+var _savedObjects := []
+
+func _init(modLoader = ModLoader):
+	l("Initializing")
+	#loadDLC()
+	installScriptExtension("hud/ShipList.gd")
+	installScriptExtension("hud/components/ShipOnOMSNameTransponder.gd")
+	updateTL("i18n/en.txt", "|")
+	#updateTL("i18n/ua.txt", "|")
+	l("Initialized")
+
+
+func updateTL(path:String, delim:String = ","):
+	path = str(modPath + path)
+	l("Adding translations from: %s" % path)
+	var tlFile:File = File.new()
+	tlFile.open(path, File.READ)
+
+	var translations := []
+
+	var csvLine := tlFile.get_line().split(delim)
+	l("Adding translations as: %s" % csvLine)
+	for i in range(1, csvLine.size()):
+		var translationObject := Translation.new()
+		translationObject.locale = csvLine[i]
+		translations.append(translationObject)
+
+	while not tlFile.eof_reached():
+		csvLine = tlFile.get_csv_line(delim)
+
+		if csvLine.size() > 1:
+			var translationID := csvLine[0]
+			for i in range(1, csvLine.size()):
+				translations[i - 1].add_message(translationID, csvLine[i].c_unescape())
+			l("Added translation: %s" % csvLine)
+
+	tlFile.close()
+
+	for translationObject in translations:
+		TranslationServer.add_translation(translationObject)
+
+	l("Translations Updated")
+
+
+# Helper function to extend scripts
+# Loads the script you pass, checks what script is extended, and overrides it
+func installScriptExtension(path:String):
+	var childPath:String = str(modPath + path)
+	var childScript:Script = ResourceLoader.load(childPath)
+
+	childScript.new()
+
+	var parentScript:Script = childScript.get_base_script()
+	var parentPath:String = parentScript.resource_path
+
+	l("Installing script extension: %s <- %s" % [parentPath, childPath])
+
+	childScript.take_over_path(parentPath)
+
+
+# Helper function to replace scenes
+# Can either be passed a single path, or two paths
+# With a single path, it will replace the vanilla scene in the same relative position
+func replaceScene(newPath:String, oldPath:String = ""):
+	l("Updating scene: %s" % newPath)
+
+	if oldPath.empty():
+		oldPath = str("res://" + newPath)
+
+	newPath = str(modPath + newPath)
+
+	var scene := load(newPath)
+	scene.take_over_path(oldPath)
+	_savedObjects.append(scene)
+	l("Finished updating: %s" % oldPath)
+
+
+# Instances Settings.gd, loads DLC, then frees the script.
+func loadDLC():
+	l("Preloading DLC as workaround")
+	var DLCLoader:Settings = preload("res://Settings.gd").new()
+	DLCLoader.loadDLC()
+	DLCLoader.queue_free()
+	l("Finished loading DLC")
+
+
+# Func to print messages to the logs
+func l(msg:String, title:String = MOD_NAME, version:String = str(MOD_VERSION_MAJOR) + "." + str(MOD_VERSION_MINOR) + "." + str(MOD_VERSION_BUGFIX)):
+	if not MOD_VERSION_METADATA == "":
+		version = version + "-" + MOD_VERSION_METADATA
+		Debug.l("[%s V%s]: %s" % [title, version, msg])
--- hud/components/ShipOnOMSNameTransponder.gd
+++ hud/components/ShipOnOMSNameTransponder.gd
@@ -1,7 +1,15 @@
 extends "res://hud/components/ShipOnOMSNameTransponder.gd"
 
 var ship_class_name_label
-var ship # Add this line
+var ship
+
+# Load the HevLib ConfigDriver
+var ConfigDriver = load("res://HevLib/pointers/ConfigDriver.gd")
+
+
+const HYBRID_SHIP_NAMES = ["SHIP_TRTL", "SHIP_PROSPECTOR", "SHIP_COTHON", "SHIP_EIME", "SHIP_TRTL"]
+const HYBRID_SHIP_MODELS = ["TRTL", "PROSPECTOR", "COTHON", "EIME", "TRTL"]
+
 
 func _ready():
 	ship_class_name_label = get_node("ShipClassName")
@@ -10,65 +18,82 @@ func setData(shipName, transponder):
 	.setData(shipName, transponder)
 	var shipObject = null
 
-	Debug.l("ShipOnOMSNameTransponder: 'ship' variable = %s" % ship)
 	if ship:
-		Debug.l("ShipOnOMSNameTransponder: ship.identifiedShips = %s" % ship.identifiedShips)
-		Debug.l("ShipOnOMSNameTransponder: searching for transponder = %s" % transponder)
-		if ship.identifiedShips.empty():
-			Debug.l("ShipOnOMSNameTransponder: ship.identifiedShips is empty!")
 		for key in ship.identifiedShips:
 			var identified_ship_data = ship.identifiedShips[key]
-			Debug.l("ShipOnOMSNameTransponder: Comparing identified_ship_data[0] (%s) with transponder (%s)" % [identified_ship_data[0], transponder])
-			if identified_ship_data[0] == transponder: # Check if the transponder matches
+			if identified_ship_data[0] == transponder:
 				shipObject = identified_ship_data[2]
-				Debug.l("ShipOnOMSNameTransponder: Found shipObject: %s" % shipObject)
 				break
 
-	Debug.l("ShipOnOMSNameTransponder: final shipObject = %s" % shipObject)
 	if ship_class_name_label and shipObject:
 		var ship_display_name = "UNKNOWN"
-		var is_hailable = false # Assume not hailable unless explicitly true
+		var is_hailable = false
+
+		var is_hybrid_ship = false
+		if shipObject.filename.ends_with("HYB-based.tscn"):
+			is_hybrid_ship = true
+
 		if shipObject.has_method("get_hailable"):
 			is_hailable = shipObject.get_hailable()
 		elif shipObject.has_method("get") and shipObject.get("hailable") != null:
 			is_hailable = shipObject.get("hailable")
-		
-		# If transponder is UIO, always treat as UNKNOWN
+
 		if transponder == "UIO":
 			ship_display_name = "UNKNOWN"
-		elif is_hailable:
+		elif is_hailable or is_hybrid_ship:
 			var potential_name = ""
-			var ship_name_property = null
-			if shipObject.has_method("get_ship_name"):
-				ship_name_property = shipObject.get_ship_name()
-			elif shipObject.has_method("get") and shipObject.get("shipName") != null:
-				ship_name_property = shipObject.get("shipName")
-			
-			if ship_name_property != null and ship_name_property != "":
-				potential_name = ship_name_property
+
+			var display_preference = ConfigDriver.__get_value("ShipTransponder", "SHIPTRANSPONDER_CONFIG_OPTIONS", "display_preference")
+
+			# HYBRID LOGIC
+			if is_hybrid_ship:
+				var random_name_key = HYBRID_SHIP_NAMES[randi() % HYBRID_SHIP_NAMES.size()]
+				var random_model = HYBRID_SHIP_MODELS[randi() % HYBRID_SHIP_MODELS.size()]
+
+				potential_name = random_name_key if display_preference == "Ship_Name" else random_model
+
+			# ALL OTHERS
 			else:
-				var model_property = null
-				if shipObject.has_method("get_model"):
-					model_property = shipObject.get_model()
-				elif shipObject.has_method("get") and shipObject.get("model") != null:
-					model_property = shipObject.get("model")
-				
-				if model_property != null and model_property != "":
-					potential_name = model_property
-			
-			# Check for translation and set display name
+				var primary_name = ""
+				var fallback_name = ""
+
+				if display_preference == "Ship_Name":
+					primary_name = get_ship_name_property(shipObject)
+					fallback_name = get_model_property(shipObject)
+				else: # Ship_Model
+					primary_name = get_model_property(shipObject)
+					fallback_name = get_ship_name_property(shipObject)
+
+				potential_name = primary_name if primary_name != "" else fallback_name
+
+			# DISPLAY
+			var show_all_names = ConfigDriver.__get_value("ShipTransponder", "SHIPTRANSPONDER_CONFIG_OPTIONS", "show_all_names")
 			if potential_name != "":
 				if potential_name.begins_with("SHIP_"):
 					var translated_name = tr(potential_name)
-					if translated_name == potential_name:
+					if translated_name == potential_name and not show_all_names:
 						ship_display_name = "UNKNOWN"
 					else:
 						ship_display_name = translated_name
 				else:
 					ship_display_name = potential_name
-			
-		Debug.l("ShipOnOMSNameTransponder: Attempting to set class name to: %s" % ship_display_name)
+
 		ship_class_name_label.set_text(ship_display_name)
 	elif ship_class_name_label:
-		Debug.l("ShipOnOMSNameTransponder: shipObject is null or not hailable, setting class name to UNKNOWN")
 		ship_class_name_label.set_text("UNKNOWN")
+
+# Helper function to get ship name
+func get_ship_name_property(obj):
+	if obj.has_method("get_ship_name"):
+		return obj.get_ship_name()
+	elif obj.has_method("get") and obj.get("shipName") != null:
+		return obj.get("shipName")
+	return ""
+
+# Helper function to get ship model
+func get_model_property(obj):
+	if obj.has_method("get_model"):
+		return obj.get_model()
+	elif obj.has_method("get") and obj.get("model") != null:
+		return obj.get("model")
+	return ""
