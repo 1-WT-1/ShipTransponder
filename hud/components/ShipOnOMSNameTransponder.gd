b8c299dd28767af443de4faa91ede0d8329550e1
WT <go.to.another5@gmail.com>
2025-10-19 12:04:47 +0300

    0.2.5
    Fix crash if object is despawned?
    Fix fallback to model if  translation string is empty.

--- hud/components/ShipOnOMSNameTransponder.gd
+++ hud/components/ShipOnOMSNameTransponder.gd
@@ -17,7 +17,6 @@ func _ready():
 func setData(shipName, transponder):
 	.setData(shipName, transponder)
 	var shipObject = null
-
 	if ship:
 		for key in ship.identifiedShips:
 			var identified_ship_data = ship.identifiedShips[key]
@@ -25,87 +24,91 @@ func setData(shipName, transponder):
 				shipObject = identified_ship_data[2]
 				break
 
-	if ship_class_name_label and shipObject:
-		var ship_display_name = "UNKNOWN"
-		var is_hailable = false
+	if Tool.claim(shipObject):
+		if ship_class_name_label and shipObject:
+			var ship_display_name = "UNKNOWN"
+			var is_hailable = false
 
-		if shipObject.has_method("get_hailable"):
-			is_hailable = shipObject.get_hailable()
-		elif shipObject.has_method("get") and shipObject.get("hailable") != null:
-			is_hailable = shipObject.get("hailable")
+			if shipObject.has_method("get_hailable"):
+				is_hailable = shipObject.get_hailable()
+			elif shipObject.has_method("get") and shipObject.get("hailable") != null:
+				is_hailable = shipObject.get("hailable")
 
-		var show_non_hailable = ConfigDriver.__get_value("ShipTransponder", "SHIPTRANSPONDER_CONFIG_OPTIONS", "show_non_hailable")
-		var ship_name_prop = get_ship_name_property(shipObject)
-		var ship_model_prop = get_model_property(shipObject)
+			var show_non_hailable = ConfigDriver.__get_value("ShipTransponder", "SHIPTRANSPONDER_CONFIG_OPTIONS", "show_non_hailable")
+			var ship_name_prop = get_ship_name_property(shipObject)
+			var ship_model_prop = get_model_property(shipObject)
 
-		# CONDITION
-		if transponder != "UIO" and (is_hailable or show_non_hailable or shipObject.name in ["HYB-base", "Tsukuyomi", "ATLAS"] or ship_name_prop in ["STATION_HABITAT", "STATION_PHAGE"]):
-			var potential_name = ""
-			var display_preference = ConfigDriver.__get_value("ShipTransponder", "SHIPTRANSPONDER_CONFIG_OPTIONS", "display_preference")
+			# CONDITION
+			if transponder != "UIO" and (is_hailable or show_non_hailable or shipObject.name in ["HYB-base", "Tsukuyomi", "ATLAS"] or ship_name_prop in ["STATION_HABITAT", "STATION_PHAGE"]):
+				var potential_name = ""
+				var display_preference = ConfigDriver.__get_value("ShipTransponder", "SHIPTRANSPONDER_CONFIG_OPTIONS", "display_preference")
 
-			if ship_name_prop == "STATION_HABITAT":
-				potential_name = "SHIP_TRANSPONDER_HABITAT_NAME" if display_preference == "Name" else "HABITAT"
-			else:
-				match shipObject.name:
-					"HYB-base":
-						var hybrid_name_key = ""
-						var hybrid_model = ""
-						var dynamic_hybrid_naming = ConfigDriver.__get_value("ShipTransponder", "SHIPTRANSPONDER_CONFIG_OPTIONS", "dynamic_hybrid_naming")
+				if ship_name_prop == "STATION_HABITAT":
+					potential_name = "SHIP_TRANSPONDER_HABITAT_NAME" if display_preference == "Name" else "HABITAT"
+				else:
+					match shipObject.name:
+						"HYB-base":
+							var hybrid_name_key = ""
+							var hybrid_model = ""
+							var dynamic_hybrid_naming = ConfigDriver.__get_value("ShipTransponder", "SHIPTRANSPONDER_CONFIG_OPTIONS", "dynamic_hybrid_naming")
 
-						if not dynamic_hybrid_naming:
-							if shipObject.has_meta("hybrid_name_key"):
-								hybrid_name_key = shipObject.get_meta("hybrid_name_key")
-								hybrid_model = shipObject.get_meta("hybrid_model")
+							if not dynamic_hybrid_naming:
+								if shipObject.has_meta("hybrid_name_key"):
+									hybrid_name_key = shipObject.get_meta("hybrid_name_key")
+									hybrid_model = shipObject.get_meta("hybrid_model")
+								else:
+									hybrid_name_key = HYBRID_SHIP_NAMES[randi() % HYBRID_SHIP_NAMES.size()]
+									hybrid_model = HYBRID_SHIP_MODELS[randi() % HYBRID_SHIP_MODELS.size()]
+									shipObject.set_meta("hybrid_name_key", hybrid_name_key)
+									shipObject.set_meta("hybrid_model", hybrid_model)
 							else:
 								hybrid_name_key = HYBRID_SHIP_NAMES[randi() % HYBRID_SHIP_NAMES.size()]
 								hybrid_model = HYBRID_SHIP_MODELS[randi() % HYBRID_SHIP_MODELS.size()]
-								shipObject.set_meta("hybrid_name_key", hybrid_name_key)
-								shipObject.set_meta("hybrid_model", hybrid_model)
+
+							potential_name = hybrid_name_key if display_preference == "Name" else hybrid_model
+
+						"Tsukuyomi":
+							potential_name = "SHIP_TRANSPONDER_TSUKUYOMI_NAME" if display_preference == "Name" else "TSUKUYOMI"
+
+						"STATION_PHAGE":
+							potential_name = "SHIP_TRANSPONDER_PHAGE_NAME" if display_preference == "Name" else "PHAGE"
+
+						"ATLAS":
+							potential_name = "SHIP_TRANSPONDER_ATLAS_NAME" if display_preference == "Name" else "ATLAS"
+
+						_: # Default case for all other ships
+							var primary_name = ""
+							var fallback_name = ""
+							if display_preference == "Name":
+								primary_name = ship_name_prop
+								fallback_name = ship_model_prop
+							else: # Model
+								primary_name = ship_model_prop
+								fallback_name = ship_name_prop
+							potential_name = primary_name if primary_name != "" else fallback_name
+
+				# DISPLAY
+				var show_all_names = ConfigDriver.__get_value("ShipTransponder", "SHIPTRANSPONDER_CONFIG_OPTIONS", "show_all_names")
+				if potential_name != "":
+					var is_translation_key = (potential_name == potential_name.to_upper() and "_" in potential_name)
+
+					if is_translation_key:
+						var translated_name = tr(potential_name)
+						if translated_name != potential_name:
+							ship_display_name = translated_name
+						elif show_all_names:
+							ship_display_name = potential_name
+						elif ship_model_prop != "":
+							ship_display_name = ship_model_prop
 						else:
-							hybrid_name_key = HYBRID_SHIP_NAMES[randi() % HYBRID_SHIP_NAMES.size()]
-							hybrid_model = HYBRID_SHIP_MODELS[randi() % HYBRID_SHIP_MODELS.size()]
-							
-						potential_name = hybrid_name_key if display_preference == "Name" else hybrid_model
-
-					"Tsukuyomi":
-						potential_name = "SHIP_TRANSPONDER_TSUKUYOMI_NAME" if display_preference == "Name" else "TSUKUYOMI"
-						
-					"STATION_PHAGE":
-						potential_name = "SHIP_TRANSPONDER_PHAGE_NAME" if display_preference == "Name" else "PHAGE"
-
-					"ATLAS":
-						potential_name = "SHIP_TRANSPONDER_ATLAS_NAME" if display_preference == "Name" else "ATLAS"
-
-					_: # Default case for all other ships
-						var primary_name = ""
-						var fallback_name = ""
-						if display_preference == "Name":
-							primary_name = ship_name_prop
-							fallback_name = ship_model_prop
-						else: # Model
-							primary_name = ship_model_prop
-							fallback_name = ship_name_prop
-						potential_name = primary_name if primary_name != "" else fallback_name
-
-			# DISPLAY
-			var show_all_names = ConfigDriver.__get_value("ShipTransponder", "SHIPTRANSPONDER_CONFIG_OPTIONS", "show_all_names")
-			if potential_name != "":
-				var is_translation_key = (potential_name == potential_name.to_upper() and "_" in potential_name)
-
-				if is_translation_key:
-					var translated_name = tr(potential_name)
-					if translated_name != potential_name:
-						ship_display_name = translated_name
-					elif show_all_names:
-						ship_display_name = potential_name
+							ship_display_name = "UNKNOWN"
 					else:
-						ship_display_name = "UNKNOWN"
-				else:
-					ship_display_name = potential_name
+						ship_display_name = potential_name
 
-		ship_class_name_label.set_text(ship_display_name)
-	elif ship_class_name_label:
-		ship_class_name_label.set_text("UNKNOWN")
+			ship_class_name_label.set_text(ship_display_name)
+		elif ship_class_name_label:
+			ship_class_name_label.set_text("UNKNOWN")
+	Tool.release(shipObject)
 
 # Helper function to get ship name
 func get_ship_name_property(obj):
