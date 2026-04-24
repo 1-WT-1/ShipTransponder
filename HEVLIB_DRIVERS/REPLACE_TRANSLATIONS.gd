5fe22246e624b86aa9fbdfeef9501aa9aa655a10
WT <go.to.another5@gmail.com>
2026-04-24 14:34:44 +0300

    v0.2.6

---
+++ HEVLIB_DRIVERS/REPLACE_TRANSLATIONS.gd
@@ -0,0 +1,76 @@
+extends Node
+
+# This translation file is generated automatically
+# Do not modify anything directly, as this can break things for those working on them
+# Please use Translation Tracker to modify these yourself, and contact the mod author to implement them
+# https://github.com/rwqfsfasxc100/TranslationTracker/releases/latest
+
+const TRANSLATIONS = {
+	"master_locale": "en",
+	"en": {
+		"SHIP_TRANSPONDER_NAME": {
+			"string": "Ship Transponder",
+			"version_hash": 95744681
+		},
+		"SHIP_TRANSPONDER_DESCRIPTION": {
+			"string": "Displays the class name of ships in the HUD.",
+			"version_hash": 849420553
+		},
+		"SHIP_TRANSPONDER_BRIEF": {
+			"string": "Adds ship class names to the OMS.",
+			"version_hash": 2892576192
+		},
+		"SHIPTRANSPONDER_CONFIG_OPTIONS": {
+			"string": "Config",
+			"version_hash": 2889832539
+		},
+		"SHIP_TRANSPONDER_CONFIG_DISPLAY_PREFERENCE_NAME": {
+			"string": "Display Preference",
+			"version_hash": 2617567098
+		},
+		"SHIP_TRANSPONDER_CONFIG_DISPLAY_PREFERENCE_DESC": {
+			"string": "Choose whether to prioritize displaying the ship's name or its model. \\nThe mod will fall back to the other option if your preferred choice is not available for a specific ship.",
+			"version_hash": 3796078909
+		},
+		"SHIP_TRANSPONDER_CONFIG_SHOW_NON_HAILABLE_NAME": {
+			"string": "Show Non-Hailable",
+			"version_hash": 3658130160
+		},
+		"SHIP_TRANSPONDER_CONFIG_SHOW_NON_HAILABLE_DESC": {
+			"string": "If enabled, the mod will attempt to display names for non-hailable targets, such as derelicts.",
+			"version_hash": 449346843
+		},
+		"SHIP_TRANSPONDER_CONFIG_SHOW_ALL_NAMES_NAME": {
+			"string": "Show Untranslated Keys",
+			"version_hash": 2075586615
+		},
+		"SHIP_TRANSPONDER_CONFIG_SHOW_ALL_NAMES_DESC": {
+			"string": "WARNING: This may display debug names. \\nIf enabled, shows the raw translation key (e.g., SHIP_INTERNAL_NAME) instead of UNKNOWN when a proper translation is not found.",
+			"version_hash": 2820750509
+		},
+		"SHIP_TRANSPONDER_CONFIG_DYNAMIC_HYBRID_NAME": {
+			"string": "Dynamic Hybrid Naming",
+			"version_hash": 1810862278
+		},
+		"SHIP_TRANSPONDER_CONFIG_DYNAMIC_HYBRID_DESC": {
+			"string": "If enabled, the Hybrid ship's mimicked identity will change frequently. \\nBy default, it is fixed for each encounter.",
+			"version_hash": 2443561500
+		},
+		"SHIP_TRANSPONDER_TSUKUYOMI_NAME": {
+			"string": "Tsukuyomi-Class Frigate",
+			"version_hash": 589197220
+		},
+		"SHIP_TRANSPONDER_HABITAT_NAME": {
+			"string": "Obonto Habitat",
+			"version_hash": 2914815539
+		},
+		"SHIP_TRANSPONDER_PHAGE_NAME": {
+			"string": "Phage Station",
+			"version_hash": 3457502732
+		},
+		"SHIP_TRANSPONDER_ATLAS_NAME": {
+			"string": "ATLAS Edge Runner",
+			"version_hash": 569846057
+		}
+	}
+}
